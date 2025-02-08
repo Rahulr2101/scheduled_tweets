029313ee0dcca4853b4a0e4fc7c42768743b13ae
olawanlejoel <rahulrajesh2101@gmail.com>
2025-02-08 16:25:36 +0530

    Add login form

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationController < ApplicationController
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(user_parms)
+    if @user.save
+      redirect_to root_path, notice: "Succefully created account"
+    else
+      flash[:alert]="Something went wrong"
+      render :new
+    end
+  end
+
+  def user_parms
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# email:string
+# pasword_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid address" }
+end
